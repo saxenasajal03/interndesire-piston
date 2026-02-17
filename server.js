3d87c9451308adaa79eb4fc042d8378e0676227c
Sajal Saxena <119779950+saxenasajal03@users.noreply.github.com>
2026-02-17 09:28:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json({ limit: "1mb" }));
 
 app.post("/run", async (req, res) => {
     try {
-        const response = await fetch("https://emkc.org/api/v2/piston/execute", {
+        const response = await fetch("https://piston--sajalsaxenagola.replit.app/api/v2/piston/execute", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(req.body)
